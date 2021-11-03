3e25d3f63fd3f0acf1cc205fe97cd5165367bacb
argiacmcc <argia.rubeo@cmcc.it>
2021-11-03 14:19:49 +0100

    test commit with gitpython

---
+++ git_python_html_monitoring.py
@@ -0,0 +1,22 @@
+import git
+#from git import Repo
+
+local_folder = '/Users/argia/cmcc/repos/forecast-html-monitoring'
+print("Repo already cloned, local folder: ", local_folder)
+# rorepo is a Repo instance pointing to the git-python repository.
+# For all you know, the first argument to Repo is a path to the repository
+# you want to work with
+repo = git.Repo(local_folder)
+#repo = Repo(self.rorepo.local_folder)
+#assert not repo.bare
+
+origin = repo.remote("origin")
+assert origin.exists()
+origin.fetch()
+#repo.git.add('--all')
+repo.git.status()
+repo.git.add(all=True)
+repo.git.status()
+repo.git.commit(message='test commit with gitpython')
+repo.git.status()
+repo.git.push()
