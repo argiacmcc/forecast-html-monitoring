4ef6a39e463c821f36964f022ff5685879f96791
sp2 <sp2@login1.cluster.net>
2022-03-15 20:00:17 +0100

    added data for 03 2022

--- git_push_updates.py
+++ git_push_updates.py
@@ -1,25 +1,21 @@
 import git
-#from git import Repo
 
 message = input("Please enter the commit message:\n")
-
-local_folder = '/Users/argia/cmcc/repos/forecast-html-monitoring'
+print(message)
+local_folder = './'
 print("Repo already cloned, local folder: ", local_folder)
 # rorepo is a Repo instance pointing to the git-python repository.
 # For all you know, the first argument to Repo is a path to the repository
 # you want to work with
 repo = git.Repo(local_folder)
-#repo = Repo(self.rorepo.local_folder)
-#assert not repo.bare
 
-origin = repo.remote("origin")
-assert origin.exists()
-origin.fetch()
-#repo.git.add('--all')
+#origin = repo.remote("origin")
+#assert origin.exists()
+#origin.fetch()
 repo.git.status()
 repo.git.add(all=True)
 repo.git.status()
-repo.git.commit(message)
+repo.git.commit('-m', message)
 print("commit ")
 repo.git.status()
 repo.git.push()
